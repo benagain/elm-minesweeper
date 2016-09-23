b1a93e2894546446ec2cc6e3871458ecb91d2db9
Ben Arnold <ben.arnold@intercede.com>
2016-09-23 17:30:14 +0100

    Remove duplication

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,10 @@
 module Main exposing (..)
 
 import Html.App as App
-import List
-import Random.Extra
-import Random
-import Set
 import View exposing (view)
-import Model exposing (..)
+import Model exposing (Model, Msg)
 import Update exposing (update)
+import Board
 
 
 main : Program Never
@@ -20,27 +17,6 @@ main =
         }
 
 
-
--- Init
-
-
 init : ( Model, Cmd Msg )
 init =
-    ( Model 0 [], Random.generate NewBoard (randomBoard 5) )
-
-
-randomBoard : Int -> Random.Generator (List Tile)
-randomBoard size =
-    Random.list (size ^ 2) bombFlip
-
-
-bombFlip : Random.Generator Tile
-bombFlip =
-    Random.map
-        (\b ->
-            if b then
-                CoveredBomb
-            else
-                CoveredClear
-        )
-        (Random.Extra.oneIn 4)
+    ( Model 0 [], Board.generate )
